fd4fc927ec0bab19384a8ae79e37e2bc4a538760
Yash Singhal <122280082+yashsingha99@users.noreply.github.com>
2025-07-11 17:21:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
