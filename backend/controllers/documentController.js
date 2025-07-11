b4ee0dc15b029e6c25c44ae0d04b43997e9134af
Yash Singhal <singhaly914@gmail.com>
2025-07-11 09:03:32 +0530

    chage process env name

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -7,7 +7,7 @@ const { GoogleGenAI } = require("@google/genai");
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY );
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY );
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
 
 const ai = new GoogleGenAI({});
